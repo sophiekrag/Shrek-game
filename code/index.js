d7839aa913febf276bc5a16da04bc634a83fa88f
Sophie Krag <sophie.9@live.nl>
2021-04-17 11:31:01 +0200

    Create circles that represent donkey and puss in boots and make them move around the canvas

--- code/index.js
+++ code/index.js
@@ -2,10 +2,10 @@ const startButton = document.getElementById('btn-start');
 
 startButton.addEventListener("click", () => {
     greateCanvas()
-    animateShrek()
+    animation()
 })
 
-
+//Creating the canvas
 const canvas = document.createElement('canvas');
 const ctx = canvas.getContext('2d')
 
@@ -19,24 +19,33 @@ const greateCanvas = () => {
     console.log('hello')
 }
 
+//Getting the elements inside the canvas
+const animation = () => {
+    requestAnimationFrame(animation);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    imgShrek()
+    donkey.update()
+    puss.update()
+}
+
+//Creating the player
 const playerShrek = {
     x: 425,
     y: 400,
     width: 70,
     height: 90,
     speed: 10,
-    moving: false,
 }
 
-
-const animateShrek = () => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+//Getting the img for the player
+const imgShrek = () => {
     const playerImg = new Image();
     playerImg.src = "..//images/Shrek.png"
     ctx.drawImage(playerImg, playerShrek.x, playerShrek.y, playerShrek.width, playerShrek.height)
-    requestAnimationFrame(animateShrek)
 }
 
+//Making the player move within the canvas using arrow keys
 const move = e => {
     if (e.keyCode === 37 && playerShrek.x > 0) {
         playerShrek.x -= playerShrek.speed;
@@ -55,3 +64,39 @@ const move = e => {
 document.addEventListener('keydown', move)
 
 
+
+//Creating circles to make puss and donkey move around the screen
+class Circle {
+    constructor(radius) {
+        this.x = Math.random() * canvas.width,
+            this.y = Math.random() * canvas.height,
+            this.dx = (Math.random() - 0.5) * 10,
+            this.dy = (Math.random() - 0.5) * 10,
+            this.radius = radius
+    }
+    update() {
+        this.draw()
+        this.move()
+    }
+    draw() {
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false)
+        ctx.strokeStyle = 'black'
+        ctx.stroke();
+    }
+    move() {
+        if (this.x + this.radius > canvas.width || this.x - this.radius < 0) {
+            this.dx = -this.dx
+        }
+
+        if (this.y + this.radius > canvas.height || this.y - this.radius < 0) {
+            this.dy = -this.dy
+        }
+
+        this.x += this.dx
+        this.y += this.dy
+    }
+}
+
+const donkey = new Circle(30)
+const puss = new Circle(50)
